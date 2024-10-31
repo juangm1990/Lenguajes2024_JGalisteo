1810dcdbba77043c99d3ecd12cf0a441ade71044
juangm1990 <jgm0106@alu.medac.es>
2024-10-31 12:53:46 +0100

    Sistema solar PNG

--- Ejercicios/formulario/contenido/formulario3.php
+++ Ejercicios/formulario/contenido/formulario3.php
@@ -1,11 +1,22 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nombre = $_POST['nombre'];
-    $email = $_POST['email'];
-    $telefono = $_POST['telefono'];
-    $sitioweb = $_POST['sitioweb'];
-    $asunto = $_POST['asunto'];
-    $mensaje = $_POST['mensaje'];
+    $nombre = htmlspecialchars($_POST['nombre']);
+    $email = htmlspecialchars($_POST['email']);
+    $telefono = htmlspecialchars($_POST['telefono']);
+    $color = htmlspecialchars($_POST['color']);
+    $sexo = htmlspecialchars($_POST['sexo']);
+    $frutas = isset($_POST['frutas']) ? $_POST['frutas'] : [];
+    $mensaje = htmlspecialchars($_POST['mensaje']);
+    
+    echo "<h1>Datos Recibidos</h1>";
+    echo "<p>Nombre: $nombre</p>";
+    echo "<p>Email: $email</p>";
+    echo "<p>Teléfono: $telefono</p>";
+    echo "<p>Color favorito: $color</p>";
+    echo "<p>Sexo: $sexo</p>";
+    echo "<p>Frutas que te gustan: " . implode(", ", $frutas) . "</p>";
+    echo "<p>Mensaje: $mensaje</p>";
+
 
     // Ejecutar en XAMPP: Coloca todos los archivos en la carpeta htdocs de XAMPP 
     // y abrir el navegador para acceder a http://localhost/nombre_de_tu_carpeta/formulario.html.
